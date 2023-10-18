a5bda261427e0ceee18e89c64ae3efd86f92056b
zac.ramsey <zac.ramsey@teamsparq.com>
2023-10-18 11:34:32 -0600

    Adds JwtAuthenticationFilter && completes implementation of SecurityConfig, adds Getter/Setter annotations in Token.java

--- src/main/java/com/teamsparq/springsecurity/entity/Token.java
+++ src/main/java/com/teamsparq/springsecurity/entity/Token.java
@@ -13,12 +13,16 @@ import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Token {
 
